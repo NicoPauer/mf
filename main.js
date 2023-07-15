a1f5b20feb19bea20e04a9fc0410d9327096250f
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-07-15 00:57:08 -0300

    Actualizar main.js
    prueba de JS.

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // Codigo principal que se encarga de generar el Martin Fierro
+
+let boton = document.getElementById("entrega");
+/* cuando se oprime el botón si los datos son válidos genera la imagen para descargar */
+window.addEventListener("click", alerta("entregado"));
