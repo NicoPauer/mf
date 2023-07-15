e52d83b3a4e29abb1293a59f2d635463fb343189
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-07-15 12:04:52 -0300

    Update main.js
    Correcion de errores en escritura y mas funcionalidad.

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 // Codigo principal que se encarga de generar el Martin Fierro
 
+// Selecciono elementos a manipular o de donde extraer informacion
 let boton = document.getElementById("entrega");
+let ternado = document.getElementById("ternado");
+let terna = document.getElementById("terna");
+let edicion = document.getElementById("edicion");
+// Defino funciones a usar
+function generar(notificacion)
+{
+ // Genera la imagen con la notificacion del ganador 
+  alert(notificacion);
+}
 /* cuando se oprime el botón si los datos son válidos genera la imagen para descargar */
-boton.addEventListener("click", alerta("entregado"));
+boton.addEventListener("click", generar("A " + ternado.value + " le dan Martín Fierro como " + terna.value + " " + edicion.value + "."));
