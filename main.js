b6bf412778f2f50c85c7d98f60f03833b621cf0b
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-07-16 23:31:30 -0300

    Actualizar main.js
    cambio a mejor visualización.

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ let boton = document.getElementById("entrega");
 let ternado = document.getElementById("ternado");
 let terna = document.getElementById("terna");
 let edicion = document.getElementById("edicion");
+let certificado = document.getElementById("martin-fierro");
 // Defino funciones a usar
 function generar(notificacion)
 {
-   alert(notificacion);
+   certificado.innerHTML = (notificacion);
 }
 /* cuando se oprime el botón si los datos son válidos genera la imagen para descargar */
 boton.addEventListener("click", () => generar("A " + ternado.value + " le dan Martín Fierro como " + terna.value + " " + edicion.value + "."));
